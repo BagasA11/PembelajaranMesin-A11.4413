ddd2ade2d4a7c8795aba3b144f2ba3279c10d0c8
unknown <111202214715@mhs.dinus.ac.id>
2024-03-23 16:48:19 +0700

    python plotting

--- praktikum-week2/index.py
+++ praktikum-week2/index.py
@@ -1,5 +1,6 @@
 import json
-
+import matplotlib.pyplot as plt
+import numpy as np
 
 def Average(data = {}, key = ""):
     # key validation
@@ -13,9 +14,10 @@ def Average(data = {}, key = ""):
     return avg
 
 def median(nums = []):
-    if not type([x for x in nums]) == int:
-        raise Exception(f"not number type")
-    
+    for x in nums:
+        if not (type(x) == int or type(x) == float):
+            raise Exception(f"{x} is not number type")
+            
     # v0 + vn / 2
     return (min(nums) + max(nums))/2
 
@@ -45,7 +47,25 @@ def GetMedian(data= {}, key = ""):
     numbers = []
     for x in data:
         numbers.append(data[x][key])
+    
     return median(numbers)
+
+def getKey(data = {}):
+    keys = []
+    for x in data:
+        keys.append(x)
+        
+    return keys
+
+def getListByKey(data = {}, key = ""):
+    
+    list = []
+    for k in data:
+        if not key in data[k]:
+            raise Exception(f"key {key} not exist")
+        
+        list.append(data[k][key])
+    return list
 
 # path to json file
 json_path = 'data.json'
@@ -60,10 +80,43 @@ try:
         max_produk_terjual = GetMax(data, "produk_terjual")
         max_jumlah_penjualan = GetMax(data, "jumlah_penjualan")
         
-        print(f"\nrata2 produk terjual {avg_produk_terjual} ")
-        print(f"rata2 penjualan {avg_penjualan} \n")
-        print(f"max produk terjual {max_produk_terjual} ")
-        print(f"max penjualan {max_jumlah_penjualan} \n")
+        min_produk_terjual = GetMin(data, "produk_terjual")
+        min_jumlah_penjualan = GetMin(data, "jumlah_penjualan")
+        
+        mid_produk_terjual = GetMedian(data, "produk_terjual")
+        mid_jumlah_penjualan = GetMedian(data, "jumlah_penjualan")
+        
+        print(f"\nrata2 produk terjual\t: {avg_produk_terjual} ")
+        print(f"rata2 penjualan\t: {avg_penjualan} \n")
+        
+        print(f"max produk terjual\t: {max_produk_terjual} ")
+        print(f"max penjualan\t: {max_jumlah_penjualan} ")
+        
+        print(f"min produk terjual\t: {min_produk_terjual} ")
+        print(f"min penjualan\t: {min_jumlah_penjualan} ")
+        
+        print(f"median produk terjual\t: {mid_produk_terjual} ")
+        print(f"median penjualan\t: {mid_jumlah_penjualan} \n")
+        print('\n')
+        
+        xpoint = np.array(getKey(data)) 
+        
+        ypoint_produkTerjual = np.array(getListByKey(data, "produk_terjual"))
+        print(ypoint_produkTerjual)
+        ypoint_totalPenjualan = np.array(getListByKey(data, "jumlah_penjualan"))
+        print(ypoint_totalPenjualan)
+        
+        # produk terjual
+        # merah
+        plt.plot(xpoint, ypoint_produkTerjual, c = 'red')
+        # total penjualan
+        # hijau
+        plt.plot(xpoint, ypoint_totalPenjualan, c = 'green')
+        
+        # set axis
+        plt.axis([0, 7, 0, max_jumlah_penjualan])
+        plt.show()
+        
 # Throw exception if fail to open file
 except FileNotFoundError:
     print(f"File '{json_path}' not found.")
